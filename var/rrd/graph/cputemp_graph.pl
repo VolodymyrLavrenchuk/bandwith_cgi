68d16620ccc8f728ef0e1dd14004a57fd642504f
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2013-08-11 01:01:42 +0400

    add cpu temperature page

---
+++ var/rrd/graph/cputemp_graph.pl
@@ -0,0 +1,9 @@
+#! /opt/csw/bin/perl
+use warnings;
+
+require "/var/rrd/common/build_graphs.pl";
+
+my @graphs_data = (["cputemp","cputemp","0000FF","","%2.1lf"]);
+my @opt = ();
+
+BuildGraphs("CPU temperature :: Intel Xeon X3440 Quad-core", "cputemp", "degrees C", \@opt, @graphs_data);
