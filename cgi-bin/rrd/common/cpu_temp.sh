62f8f0956a5cc723e7d99e36a3f748463ae67478
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2013-12-07 15:18:58 +0400

    fix unchanged directories after refactor

--- cgi-bin/rrd/common/cpu_temp.sh
+++ cgi-bin/rrd/common/cpu_temp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ipmitool -c -I lan -H 192.168.1.112 -f /var/rrd/common/xeon.psw -U root sdr list | grep "P1 Therm Margin" | awk -F, '{sum = $2 + 73; printf "%s",sum}'
+ipmitool -c -I lan -H 192.168.1.112 -f /www/cgi-bin/rrd/common/xeon.psw -U root sdr list | grep "P1 Therm Margin" | awk -F, '{sum = $2 + 73; printf "%s",sum}'
--- cgi-bin/rrd/update_data.pl
+++ cgi-bin/rrd/update_data.pl
@@ -1,6 +1,6 @@
 #! /opt/csw/bin/perl
 
-system ("/var/rrd/data/hddtemp_data.pl");
-system ("/var/rrd/data/traffic_data.pl");
-system ("/var/rrd/data/cpu_load_data.pl");
-system ("/var/rrd/data/cpu_temp_data.pl");
+system ("/www/cgi-bin/rrd/data/hddtemp_data.pl");
+system ("/www/cgi-bin/rrd/data/traffic_data.pl");
+system ("/www/cgi-bin/rrd/data/cpu_load_data.pl");
+system ("/www/cgi-bin/rrd/data/cpu_temp_data.pl");
