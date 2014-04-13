11df079b84ce3a0d4e5a7c5a908daba25c2a18d2
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2014-04-13 21:56:27 +0400

    fix syntax error

--- cgi-bin/rrd/common/network_list.pl
+++ cgi-bin/rrd/common/network_list.pl
@@ -4,8 +4,8 @@
             {ip_address => "192_168_1_1", name => "Asus RT-N16 ",number => "1",color_in => "FF0000",color_out => "F59090"},
             {ip_address => "192_168_1_114", name => "Scorpion-oi ",number => "2",color_in => "00FF00",color_out => "00FFFF"},
             {ip_address => "192_168_1_111", name => "Scorpion-oi2",number => "3",color_in => "336600",color_out => "0033CC"},
-            {ip_address => "192_168_1_107", name => "Scorpion-c2d",number => "4",color_in => "CC00CC"},
+            {i_address => "192_168_1_107", name => "Scorpion-c2d",number => "4",color_in => "CC00CC"},
             {ip_address => "192_168_1_105", name => "Masha-nb-lan",number => "5",color_in => "FFD4D4"},
             {ip_address => "192_168_1_115", name => "Olesja      ",number => "6",color_in => "FFFF00"},
             {ip_address => "192_168_1_103", name => "Asus O-Play ",number => "7",color_in => "F2B646"}
-            );rename network_list.pl -> traffic_list.pl
+            );
