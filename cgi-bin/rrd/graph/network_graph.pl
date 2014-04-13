508a218bfaae33f8fb4303f0ec4ab04a058af026
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2014-04-14 00:19:47 +0400

    change colors for network graph

--- cgi-bin/rrd/graph/network_graph.pl
+++ cgi-bin/rrd/graph/network_graph.pl
@@ -12,7 +12,10 @@ for $port (@ports){
     {
         push @graphs_data,[$port->{ip_address},"in",$port->{color_in},"$port->{name} in ","%5.1lf"];
     }
-    push @graphs_data,[$port->{ip_address},"out",$port->{color_out},"$port->{name} out","%5.1lf"];
+    if ($port->{color_out})
+    {
+        push @graphs_data,[$port->{ip_address},"out",$port->{color_out},"$port->{name} out","%5.1lf"];
+    }
 }
 my @opt = ("-l 0");
 BuildGraphs("Local network traffic", "network", "bytes/sec",\@opt,@graphs_data);
