b41ebfa2f392be6d90b0edd33972b022b62b0b8e
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2017-11-06 17:00:05 +0300

    restore data to db from xmls

--- cgi-bin/rrd/update_data.pl
+++ cgi-bin/rrd/update_data.pl
@@ -1,7 +1,10 @@
 #! /opt/csw/bin/perl
+use RRDs;
 
 my ($cur_dir) = __FILE__ =~ m{^(.*)/};
 my ($data_dir) = "$cur_dir/data";
+
+my $scorpionws_dir = '/mnt/dbs/monitoring/scorpion-ws';
 
 opendir my $dir, $data_dir or die "Cannot open directory: $!";
 my @modules = readdir $dir;
@@ -10,5 +13,16 @@ closedir $dir;
 foreach (@modules){
     if (-f $data_dir . "/" . $_ ){
         require "$data_dir/$_";
+    }
+}
+
+opendir my $xml_dir, "$scorpionws_dir/xmls" or die "Cannot open directory: $!";
+my @xmls = readdir $xml_dir;
+closedir $xml_dir;
+foreach (@xmls){
+    if (-f "$scorpionws_dir/xmls" . "/" . $_ ){
+      my ($filename) = $_ =~ m{^(.*)\.};
+      print "processing: $scorpionws_dir/xmls/$_\n";
+      RRDs::restore "$scorpionws_dir/xmls/$_", "$scorpionws_dir/$filename.rrd", "-f";
     }
 }
