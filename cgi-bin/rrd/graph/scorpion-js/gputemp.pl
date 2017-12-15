5c49b882407b3b223113693f0a716e878268c054
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2017-12-16 02:00:22 +0300

    temperature graph for all scorpion-js gpus

--- cgi-bin/rrd/graph/scorpion-js/gputemp.pl
+++ cgi-bin/rrd/graph/scorpion-js/gputemp.pl
@@ -5,7 +5,14 @@ use File::Basename;
 my ($cur_dir) = __FILE__ =~ m{^(.*)/};
 require "$cur_dir/../../common/build_graphs.pl";
 
-my @graphs_data = (["gpu0","gputemp","1","0000FF","","%2.1lf"]);
+my @gpu_colors = ("A8907A", "F8D9AF", "CC00CC", "00FF00", "0000FF", "00FFFF");
+
+my @graphs_data = ();
+
+for my $index (0 .. $#gpu_colors) {
+  push @graphs_data,["gpu$index","gputemp","1",$gpu_colors[$index],"GPU$index","%2.1lf"]
+}
+
 my @opt = ();
 
 BuildGraphs(basename($cur_dir), "GPU temperature :: Palit JetStream GeForce GTX 1070ti", "gputemp", "degrees C", \@opt, @graphs_data);
