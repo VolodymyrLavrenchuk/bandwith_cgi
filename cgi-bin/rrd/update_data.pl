aac3903fcaca554a23e10f9743fde495003a388f
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2017-12-16 01:36:10 +0300

    update data for multiple hosts from array~

--- cgi-bin/rrd/update_data.pl
+++ cgi-bin/rrd/update_data.pl
@@ -4,7 +4,7 @@ use RRDs;
 my ($cur_dir) = __FILE__ =~ m{^(.*)/};
 my ($data_dir) = "$cur_dir/data";
 
-my $scorpionws_dir = '/mnt/dbs/monitoring/scorpion-ws';
+my @hosts = ('scorpion-ws','scorpion-js');
 
 opendir my $dir, $data_dir or die "Cannot open directory: $!";
 my @modules = readdir $dir;
@@ -16,13 +16,17 @@ foreach (@modules){
     }
 }
 
-opendir my $xml_dir, "$scorpionws_dir/xmls" or die "Cannot open directory: $!";
-my @xmls = readdir $xml_dir;
-closedir $xml_dir;
-foreach (@xmls){
-    if (-f "$scorpionws_dir/xmls" . "/" . $_ ){
-      my ($filename) = $_ =~ m{^(.*)\.};
-      print "processing: $scorpionws_dir/xmls/$_\n";
-      RRDs::restore "$scorpionws_dir/xmls/$_", "$scorpionws_dir/$filename.rrd", "-f";
-    }
+foreach (@hosts){
+  my $cur_dir = "/mnt/dbs/monitoring/$_";
+
+  opendir my $xml_dir, "$cur_dir/xmls" or die "Cannot open directory: $!";
+  my @xmls = readdir $xml_dir;
+  closedir $xml_dir;
+  foreach (@xmls){
+      if (-f "$cur_dir/xmls" . "/" . $_ ){
+        my ($filename) = $_ =~ m{^(.*)\.};
+        print "processing: $cur_dir/xmls/$_\n";
+        RRDs::restore "$cur_dir/xmls/$_", "$cur_dir/$filename.rrd", "-f";
+      }
+  }
 }
