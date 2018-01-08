06df80e25f52a51bde287e44cefd4e9fa1c7f2d7
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2018-01-08 18:33:05 +0300

    use common code for scorpion-ws gpupower graph

--- cgi-bin/rrd/graph/scorpion-ws/gpupower.pl
+++ cgi-bin/rrd/graph/scorpion-ws/gpupower.pl
@@ -3,11 +3,8 @@ use warnings;
 use File::Basename;
 
 my ($cur_dir) = __FILE__ =~ m{^(.*)/};
-require "$cur_dir/../../common/build_graphs.pl";
+require "$cur_dir/../../common/build_gpu_graphs.pl";
 
-my @graphs_data = (["gpu0","gpupower","1","0000FF","","%2.1lf"]);
-my @opt = ("-l 0");
-
-BuildPeriodsGraphs(basename($cur_dir), "GPU power usage :: MSI GeForce GTX 1060", "gpupower", "Watts", \@opt, @graphs_data);
+BuildGPUGraphs(basename($cur_dir), "GPU power usage :: MSI GeForce GTX 1060", "Watts", "gpupower", "%2.1lf", ("-l 0"));
 
 1;
