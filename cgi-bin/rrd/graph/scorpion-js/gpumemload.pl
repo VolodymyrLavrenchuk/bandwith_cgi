93d2907c9dcbf25d80081f76bcabd29f0b3f05dd
Vladimir Lavrenchuk <Volodymyr.Lavrenchuk@quest.com>
2018-03-05 01:20:31 +0300

    graphs -> gpugraphs for js farm

--- cgi-bin/rrd/graph/scorpion-js/gpumemload.pl
+++ cgi-bin/rrd/graph/scorpion-js/gpumemload.pl
@@ -5,6 +5,6 @@ use File::Basename;
 my ($cur_dir) = __FILE__ =~ m{^(.*)/};
 require "$cur_dir/../../common/build_gpu_graphs.pl";
 
-BuildGPUGraphs(basename($cur_dir), "GPU fan spped :: Palit JetStream GeForce GTX 1070Ti", "%%", "fanspeed", "%3.1lf", ("-l 0"));
+BuildGPUGraphs(basename($cur_dir), "GPU memory load :: Palit JetStream GeForce GTX 1070Ti", "%%", "gpumemload", "memload", "%3.1lf", ("-l 0"));
 
 1;
